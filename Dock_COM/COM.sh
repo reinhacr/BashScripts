5df9c32b7dcaadd30c08205c9510657085556ba0
Clorice Reinhardt <35904761+reinhacr@users.noreply.github.com>
2023-02-24 13:23:51 -0500

    Update COM.sh
    added some comments so it was less sloppy

--- Dock_COM/COM.sh
+++ Dock_COM/COM.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Bash code to calculate R between average coordinate of cage and average coordinate of TMC, and then translate TMC coordinates to superimpose averaged coordinates and finally create combined file
 # calculate averaged coordinate of cage
+# I have a much more streamlined version of this code now which cleans up its temporary files and passes in tmc names/etc from arguments passed to code
 awk 'NR > 2 {print $1"\t"$2"\t"$3"\t"$4}' just_cage.xyz > just_cage_trimmed
 awk '{for(i=1;i<=NF;i++) {sum[i] += $i; sumsq[i] += ($i)^2}}
 END {for (i=1;i<=NF;i++) {
@@ -13,7 +14,7 @@ echo $xcage
 echo $ycage
 echo $zcage
 
-
+# Now for the TMC of interest
 awk 'NR > 1 {print $1"\t"$2"\t"$3"\t"$4}' sqr_pln9_cleaned.xyz | sed '$d' > coords
 awk '{for(i=1;i<=NF;i++) {sum[i] += $i; sumsq[i] += ($i)^2}}
 END {for (i=1;i<=NF;i++) {
@@ -26,7 +27,7 @@ echo $xcoords
 echo $ycoords
 echo $zcoords
 
-
+# Use awk to put it together, assign variables with -v, using awk since it can do floating point operations
 awk -v xcage="$xcage" -v xcoords="$xcoords" -v ycage="$ycage" -v ycoords="$ycoords" -v zcage="$zcage" -v zcoords="$zcoords" 'NR > 1 {print $1"\t"($2+(xcage-xcoords))"\t"($3+(ycage-ycoords))"\t"($4+(zcage-zcoords))}' coords > new_sqr_pln9.xyz
 cat sqr_pln9_cleaned.xyz
 cat new_sqr_pln9.xyz
@@ -34,4 +35,4 @@ cat just_cage_trimmed new_sqr_pln9.xyz | wc -l
 read number_coords <<< $( cat just_cage_trimmed new_sqr_pln9.xyz | wc -l )
 echo "$number_coords
 combined_file" > header
-cat header just_cage_trimmed new_sqr_pln9.xyz > docked_text.xyz 
+cat header just_cage_trimmed new_sqr_pln9.xyz > docked_test.xyz 
